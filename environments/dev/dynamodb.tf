0601d3843fdca94da4daa8b9b343e079d795cb7d
unknown <shafeek.m@hotmail.com>
2025-07-28 10:47:44 +0530

    Skip DynamoDB table creation using count

--- environments/dev/dynamodb.tf
+++ environments/dev/dynamodb.tf
@@ -1,4 +1,10 @@
+variable "create_lock_table" {
+  type    = bool
+  default = false
+}
+
 resource "aws_dynamodb_table" "terraform_locks" {
+  count        = var.create_lock_table ? 1 : 0
   name         = "terraform-state-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
@@ -6,5 +12,9 @@ resource "aws_dynamodb_table" "terraform_locks" {
   attribute {
     name = "LockID"
     type = "S"
+  }
+
+  tags = {
+    Name = "Terraform State Lock Table"
   }
 }
